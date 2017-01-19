44fac87c7921af021919162e32782b0183382341
Lennon Santos <lennonsbueno@gmail.com>
2017-01-19 01:07:20 -0200

    initial commit

---
+++ public/app.js
@@ -0,0 +1,32 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDzif7BOOYgESIuvEl45CXspf9iyTO_5yE",
+  authDomain: "fir-2eaf7.firebaseapp.com",
+  databaseURL: "https://fir-2eaf7.firebaseio.com",
+  storageBucket: "fir-2eaf7.appspot.com",
+  messagingSenderId: "355157793688"
+};
+firebase.initializeApp(config);
+
+var storageRef = firebase.storage().ref();
+
+var app  = angular.module("myApp", ["firebase"]);
+
+ app.controller("CtrlList", function($scope, $firebaseArray){
+
+  var ref = firebase.database().ref().child("posts");
+
+  $scope.posts = $firebaseArray(ref);
+
+  $scope.addPost = function(){          
+    $scope.posts.$add({
+      title: $scope.newTitle,
+      author: $scope.newAuthor,
+      text: $scope.newText
+    });  
+    $scope.newTitle = null; 
+    $scope.newAuthor = null; 
+    $scope.newText = null;        
+  };
+
+});
